1afc3109d92a5ef3fc1eb1108d2be76bf258c56c
Raki Rahman <46581776+mdrakiburrahman@users.noreply.github.com>
2021-06-10 14:56:42 -0400

    Added new DLL

--- ssis-ir-standard-setup/main.cmd
+++ ssis-ir-standard-setup/main.cmd
@@ -23,4 +23,27 @@ echo Failed with ExitCode %ERRORLEVEL%
 exit /b %ERRORLEVEL%)
 echo "Successfully installed Azure.Storage.Blobs.dll"
 
+REM Azure.Storage.Blobs.dll
+
+xcopy /F /Y Azure.Storage.Common.dll "%ProgramFiles%\Microsoft SQL Server\140\DTS\Tasks"
+REM error handling
+if %ERRORLEVEL% neq 0 ( 
+echo Failed with ExitCode %ERRORLEVEL%
+exit /b %ERRORLEVEL%)
+echo "Complete copied to x64 folder"
+
+xcopy /F /Y Azure.Storage.Common.dll "%ProgramFiles(x86)%\Microsoft SQL Server\140\DTS\Tasks"
+REM error handling
+if %ERRORLEVEL% neq 0 ( 
+echo Failed with ExitCode %ERRORLEVEL%
+exit /b %ERRORLEVEL%)
+echo "Complete copied to x86 folder"
+
+gacutil\gacutil /i Azure.Storage.Common.dll /f
+REM error handling
+if %ERRORLEVEL% neq 0 ( 
+echo Failed with ExitCode %ERRORLEVEL%
+exit /b %ERRORLEVEL%)
+echo "Successfully installed Azure.Storage.Common.dll"
+
 REM ODBC 17...TBD
