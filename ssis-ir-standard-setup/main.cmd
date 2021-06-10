97f85cbd725cd7d6fe34ac58074c6dc9ec4e9206
Raki Rahman <46581776+mdrakiburrahman@users.noreply.github.com>
2021-06-10 15:12:22 -0400

    New DLL

--- ssis-ir-standard-setup/main.cmd
+++ ssis-ir-standard-setup/main.cmd
@@ -2,48 +2,25 @@
 
 REM Azure.Storage.Blobs.dll
 
-xcopy /F /Y Azure.Storage.Blobs.dll "%ProgramFiles%\Microsoft SQL Server\140\DTS\Tasks"
+xcopy /F /Y Microsoft.WindowsAzure.Storage.dll "%ProgramFiles%\Microsoft SQL Server\140\DTS\Tasks"
 REM error handling
 if %ERRORLEVEL% neq 0 ( 
 echo Failed with ExitCode %ERRORLEVEL%
 exit /b %ERRORLEVEL%)
 echo "Complete copied to x64 folder"
 
-xcopy /F /Y Azure.Storage.Blobs.dll "%ProgramFiles(x86)%\Microsoft SQL Server\140\DTS\Tasks"
+xcopy /F /Y Microsoft.WindowsAzure.Storage.dll "%ProgramFiles(x86)%\Microsoft SQL Server\140\DTS\Tasks"
 REM error handling
 if %ERRORLEVEL% neq 0 ( 
 echo Failed with ExitCode %ERRORLEVEL%
 exit /b %ERRORLEVEL%)
 echo "Complete copied to x86 folder"
 
-gacutil\gacutil /i Azure.Storage.Blobs.dll /f
+gacutil\gacutil /i Microsoft.WindowsAzure.Storage.dll /f
 REM error handling
 if %ERRORLEVEL% neq 0 ( 
 echo Failed with ExitCode %ERRORLEVEL%
 exit /b %ERRORLEVEL%)
-echo "Successfully installed Azure.Storage.Blobs.dll"
-
-REM Azure.Storage.Blobs.dll
-
-xcopy /F /Y Azure.Storage.Common.dll "%ProgramFiles%\Microsoft SQL Server\140\DTS\Tasks"
-REM error handling
-if %ERRORLEVEL% neq 0 ( 
-echo Failed with ExitCode %ERRORLEVEL%
-exit /b %ERRORLEVEL%)
-echo "Complete copied to x64 folder"
-
-xcopy /F /Y Azure.Storage.Common.dll "%ProgramFiles(x86)%\Microsoft SQL Server\140\DTS\Tasks"
-REM error handling
-if %ERRORLEVEL% neq 0 ( 
-echo Failed with ExitCode %ERRORLEVEL%
-exit /b %ERRORLEVEL%)
-echo "Complete copied to x86 folder"
-
-gacutil\gacutil /i Azure.Storage.Common.dll /f
-REM error handling
-if %ERRORLEVEL% neq 0 ( 
-echo Failed with ExitCode %ERRORLEVEL%
-exit /b %ERRORLEVEL%)
-echo "Successfully installed Azure.Storage.Common.dll"
+echo "Successfully installed Microsoft.WindowsAzure.Storage.dll"
 
 REM ODBC 17...TBD
