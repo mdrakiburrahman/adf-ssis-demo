e367043c511f36cad283b5acb3e693940c09d076
Raki Rahman <46581776+mdrakiburrahman@users.noreply.github.com>
2021-06-10 14:27:45 -0400

    Included custom DLL logic

---
+++ ssis-ir-standard-setup/main.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+REM Azure.Storage.Blobs.dll
+
+xcopy /F /Y Azure.Storage.Blobs.dll "%ProgramFiles%\Microsoft SQL Server\140\DTS\Tasks"
+REM error handling
+if %ERRORLEVEL% neq 0 ( 
+echo Failed with ExitCode %ERRORLEVEL%
+exit /b %ERRORLEVEL%)
+echo "Complete copied to x64 folder"
+
+xcopy /F /Y Azure.Storage.Blobs.dll "%ProgramFiles(x86)%\Microsoft SQL Server\140\DTS\Tasks"
+REM error handling
+if %ERRORLEVEL% neq 0 ( 
+echo Failed with ExitCode %ERRORLEVEL%
+exit /b %ERRORLEVEL%)
+echo "Complete copied to x86 folder"
+
+gacutil\gacutil /i Azure.Storage.Blobs.dll /f
+REM error handling
+if %ERRORLEVEL% neq 0 ( 
+echo Failed with ExitCode %ERRORLEVEL%
+exit /b %ERRORLEVEL%)
+echo "Successfully installed Azure.Storage.Blobs.dll"
+
+REM ODBC 17...TBD
